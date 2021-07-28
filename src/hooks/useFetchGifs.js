9a57f7aa099388e83461a3f8b1b39e43a732f4f7
Georgy <georgy.1010.011@gmail.com>
2021-07-28 02:08:26 -0500

    first commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react"
+import { getGifs } from "../helpers/getGifs";
+
+
+export const useFetchGifs = ( category ) => {
+
+    const [state, setState] = useState({
+        data: [],
+        loading: true
+    });
+
+    useEffect(() => {
+        
+        getGifs( category )
+            .then( gifs => {
+
+                // setTimeout( () => {
+
+                    setState({
+                        data: gifs,
+                        loading: false
+                    })
+
+                // }, 500 );
+
+            })
+
+    }, [category]);
+
+    return state;
+
+}
